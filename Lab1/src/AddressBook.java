80853dafd9432284c79bc5e7b14f293dfa85d159
d_god <d_godfrey1@aol.com>
2023-09-18 16:35:44 -0400

    Changed main method to add and remove a buddy from an address book.

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -10,6 +10,9 @@ public class AddressBook {
         buddies.remove(x);
     }
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
